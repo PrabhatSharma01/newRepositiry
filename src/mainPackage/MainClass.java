0981bed5ba38f0e0b5b37862e7e0de39ebce9564
Prabhat Sharma <vegetasama0905@gmail.com>
2022-09-22 14:36:02 +0530

    MyClass.class changes

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass{
 	public static void main(String[] args) {
 		
 		System.out.println("This is created in Eclipse");
+		System.out.println("This is added in GitBash");
 		
 	}
 	
